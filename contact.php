2ecaf6c5a281f2abb50b5c844ae8ce2ea7dedbc6
Kangji-Chen <43071113+Kangji-Chen@users.noreply.github.com>
2020-05-25 22:33:18 -0500

    third

--- contact.php
+++ contact.php
@@ -1,11 +1,10 @@
 <?php
-    $name = $_REQUEST['name'];
-    $visitor_email = $_REQUEST['email'];
-    $message = $_REQUEST['message'];
+    $name = $_POST['name'];
+    $visitor_email = $_POST['Email'];
+    $message = $_POST['Message'];
 
     mail("kangji1999@utexas.edu","Personal Website Message",$message,"From: $name<$visitor_email>");
     echo"<script type = 'text/javascript'>alert('your message sent successfully')
-    window.history.log(-1);
     </script>";
 
 ?>
